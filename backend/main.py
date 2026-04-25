a2b364fee214c1022107a130a24a5113781a1480
KadenC707 <kcase14@lsu.edu>
2026-04-25 15:04:16 -0500

    Changed routing to correct file name

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,9 @@
 #initializes fastapi and registers main router
 
-from fastapi import FastAPI, APIRouter
+from fastapi import APIRouter, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import test,players,teams
+from routers import test,players,teams,search
 from routers import players_info, predict, team_info
-from routers import players_info,predict
 
 
 app = FastAPI()
@@ -16,15 +15,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 main_router = APIRouter(prefix="/api")
-main_router.include_router(players_info.router)
-main_router.include_router(predict.router)
-
-app.include_router(main_router)
 app.include_router(test.router)
 app.include_router(players.router)
 app.include_router(players_info.router)
 app.include_router(predict.router)
 app.include_router(team_info.router)
+app.include_router(search.router)
 
 @app.get("/")
 def root():

527f7b47a89db01578e9cdedfe30da2e69248b8d
KadenC707 <kcase14@lsu.edu>
2026-04-28 20:41:18 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,7 @@
 
 from fastapi import APIRouter, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import test,players,teams,search
-from routers import players_info, predict, team_info
+from routers import players_info, predict, team_info, search
 
 
 app = FastAPI()
@@ -21,8 +20,6 @@ main_router.include_router(team_info.router)
 main_router.include_router(search.router)
 
 app.include_router(main_router)
-app.include_router(test.router)
-app.include_router(players.router)
 app.include_router(players_info.router)
 app.include_router(predict.router)
 app.include_router(team_info.router)

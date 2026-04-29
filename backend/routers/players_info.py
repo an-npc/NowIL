d87e7c0a823d0a809788ca3a027bdad7eb2afbe4
walthe219 <walthe219@gmail.com>
2026-04-28 21:14:31 -0500

    removed duplicate code

--- backend/routers/players_info.py
+++ backend/routers/players_info.py
@@ -5,7 +5,6 @@ from database import get_session
 
 from models.database_tables import Player, Performance, Team, Game
 from models.player import PlayerTableData
-from routers.shared import get_current_nil_subquery
 from sqlmodel import Session, select, col,func, desc 
 from database import get_session
 from models.database_tables import Player, Performance,Team, Game
@@ -18,14 +17,12 @@ def get_players_data(
         team_id:int|None = None,
         position:str|None = None,
         college_year:str|None = None,
-        limit:int = 1,
         limit:int = 10,
         offset:int = 0,
         session:Session = Depends(get_session)
     ):
     
     # Use shared subquery to get current NIL values for each player
-    current_nils = get_current_nil_subquery()
     # 1. Subquery to rank performances per player by date
     # We use row_number() to pick the single newest record
     subq = (
